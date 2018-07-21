3ee9b12f60a2216fc57aed1660973f7e2c8e2aa9
Alicia D <aliciakds88@gmail.com>
2018-07-21 16:05:49 -0700

    Adding State machine interfaces and comment parsing

---
+++ TwitchChatBot/IGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchChatBot
+{
+    /// <summary>
+    /// IGame represents the Context object in our State pattern
+    /// </summary>
+    interface IGame
+    {
+        IGameState State { get; set; }
+    }
+}
---
+++ TwitchChatBot/IGameState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchChatBot
+{
+    /// <summary>
+    /// IGameState represents the State interface in our State pattern.
+    /// </summary>
+    interface IGameState
+    {
+        /// <summary>
+        /// Register a player for the game, if the current IGameState permits.  We should probably let players know if
+        /// they try to register when registrations aren't allowed.
+        /// </summary>
+        /// <param name="player">Player trying to register</param>
+        void Register(IPlayer player);
+    }
+}
